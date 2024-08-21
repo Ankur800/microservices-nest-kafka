f98401bb380c49e1eb71d22e00ac80a43f8a3794
Ankur Rai <ankurrai@Ankurs-MacBook-Pro.local>
2024-08-21 15:54:01 +0530

    Microservices with nestjs and kafka

---
+++ auth/src/app.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { GetUserRequest } from './get-user-request.dto';
+
+@Injectable()
+export class AppService {
+  private readonly users: any[] = [
+    {
+      userId: '123',
+      paytmId: 'PAYTM123',
+    },
+    {
+      userId: '234',
+      paytmId: 'PAYTM234',
+    },
+  ];
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getUser(getUserRequest: GetUserRequest) {
+    return this.users.find((user) => user.userId === getUserRequest.userId);
+  }
+}
